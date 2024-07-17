b2069b17df509d2e4d019628f2306365d2030f1e
knmsh08200 <ivanov.vladimir08200@mail.ru>
2024-07-17 03:09:50 +0300

    [main] blog_test

--- blog.go
+++ blog.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"strconv"
 	"strings"
 	"sync"
+
+	_ "github.com/lib/pq"
 )
 
 type List struct {
 	ID      int    `json:"id"`
+	USER_ID int    `json:"user_id"`
 	Title   string `json:"title"`
 	Content string `json:"content"`
 }
@@ -22,25 +27,77 @@ type ID struct {
 }
 
 var (
-	lists = []List{
-		{ID: 1, Title: "First Blog", Content: "This is the first blog post."},
-		{ID: 2, Title: "Second Blog", Content: "This is the second blog post."},
-	}
-	ids = []ID{
-		{ID: 1, Name: "Author One"},
-		{ID: 2, Name: "Author Two"},
-	}
+	// lists = []List{
+	// 	{ID: 1, Title: "First Blog", Content: "This is the first blog post."},
+	// 	{ID: 2, Title: "Second Blog", Content: "This is the second blog post."},
+	// }
+	// ids = []ID{
+	// 	{ID: 1, Name: "Author One"},
+	// 	{ID: 2, Name: "Author Two"},
+	// }
 	mutex = &sync.Mutex{}
 )
 
+var db *sql.DB
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./static")))
+	var err error
+	db, err = sql.Open("postgres", "user=postgres  password=1963ctvmz dbname=db_id sslmode=disable")
+	if err != nil {
+		log.Fatal("Error connecting to the database:", err)
+	}
+	defer db.Close()
+	// http.Handle("/", http.FileServer(http.Dir("./static")))
 
-	http.HandleFunc("/blog/list", blogListHandler)
-	http.HandleFunc("/blog/id", blogIDHandler)
+	http.HandleFunc("/blog/list/", blogListHandler)
+	http.HandleFunc("/blog/id/", blogIDHandler)
+	http.HandleFunc("/blog/id/counter", blogCounterHandler)
 
-	log.Println("Server is listening on port 3000...")
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	log.Println("Server is listening on port 3001...")
+	log.Fatal(http.ListenAndServe(":3001", nil))
+}
+
+func blogCounterHandler(w http.ResponseWriter, r *http.Request) {
+	// Получаем параметры из URL-запроса
+	params := r.URL.Query()
+	userIDStr := params.Get("user_id")
+
+	if userIDStr == "" {
+		http.Error(w, "User ID parameter is required", http.StatusBadRequest)
+		return
+	}
+
+	// Преобразуем userIDStr в целочисленный тип
+	userID, err := strconv.Atoi(userIDStr)
+	if err != nil {
+		http.Error(w, "Invalid user ID", http.StatusBadRequest)
+		return
+	}
+
+	// Запрос для подсчета количества статей пользователя
+	var articlesCount int
+	err = db.QueryRow("SELECT COUNT(*) FROM lists WHERE user_id=$1", userID).Scan(&articlesCount)
+	if err != nil {
+		http.Error(w, "Error fetching user articles count", http.StatusInternalServerError)
+		return
+	}
+
+	// Формируем JSON-ответ с количеством статей пользователя
+	jsonResponse := map[string]interface{}{
+		"user_id":        userID,
+		"articles_count": articlesCount,
+	}
+
+	jsonData, err := json.Marshal(jsonResponse)
+	if err != nil {
+		http.Error(w, "Error marshalling JSON", http.StatusInternalServerError)
+		return
+	}
+
+	// Отправляем JSON-ответ клиенту
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonData)
 }
 
 func blogListHandler(w http.ResponseWriter, r *http.Request) {
@@ -60,9 +117,29 @@ func blogListHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleGetList(w http.ResponseWriter) {
+	log.Println("received request to get ")
 	// w.Header().Set("Access-Control-Allow-Origin", "*")
 	// w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	// w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	var lists []List
+	rows, err := db.Query("SELECT id,user_id,title,content FROM lists")
+	if err != nil {
+		log.Printf("Error querying database: %v", err)
+		http.Error(w, "Error fetching data", http.StatusInternalServerError)
+		return
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		var list List
+		if err := rows.Scan(&list.ID, &list.USER_ID, &list.Title, &list.Content); err != nil {
+			log.Printf("error: %v", err)
+			http.Error(w, "Error scanning data", http.StatusInternalServerError)
+			return
+		}
+		lists = append(lists, list)
+	}
+
 	jsonData, err := json.Marshal(lists)
 	if err != nil {
 		http.Error(w, "Error marshalling JSON", http.StatusInternalServerError)
@@ -75,9 +152,13 @@ func handleGetList(w http.ResponseWriter) {
 }
 
 func handlePostList(w http.ResponseWriter, r *http.Request) {
+
+	log.Println("Received request to add new list")
+
 	// w.Header().Set("Access-Control-Allow-Origin", "*")
 	// w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	// w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "Error reading request body", http.StatusInternalServerError)
@@ -90,10 +171,27 @@ func handlePostList(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	mutex.Lock()
-	newList.ID = len(lists) + 1
-	lists = append(lists, newList)
-	mutex.Unlock()
+	var userExists bool
+	err = db.QueryRow("SELECT EXISTS(SELECT 1 FROM users WHERE id=$1)", newList.USER_ID).Scan(&userExists)
+	if err != nil {
+		http.Error(w, "Error checking user existence", http.StatusInternalServerError)
+		return
+	}
+
+	if !userExists {
+		http.Error(w, "User not found", http.StatusBadRequest)
+		return
+	}
+	err = db.QueryRow("INSERT INTO lists (user_id,title, content) VALUES ($1, $2, $3) RETURNING id", newList.USER_ID, newList.Title, newList.Content).Scan(&newList.ID)
+	if err != nil {
+		log.Printf("Error inserting data: %v", err)
+		http.Error(w, "Error inserting data", http.StatusInternalServerError)
+		return
+	}
+	// mutex.Lock()
+	// newList.ID = len(lists) + 1
+	// lists = append(lists, newList)
+	// mutex.Unlock()
 
 	w.WriteHeader(http.StatusCreated)
 }
@@ -109,31 +207,47 @@ func handleDelList(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	result, err := db.Exec("DELETE FROM lists WHERE id = $1", id)
+	if err != nil {
+		log.Printf("Error deleting data: %v", err)
+		http.Error(w, "Error deleting data", http.StatusInternalServerError)
+		return
+	}
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		http.Error(w, "Error getting rows affected", http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(rowsAffected)
 	mutex.Lock()
 	defer mutex.Unlock()
 
-	for i, list := range lists {
-		if list.ID == id {
-			lists = append(lists[:i], lists[i+1:]...)
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-	}
+	// for i, list := range lists {
+	// 	if list.ID == id {
+	// 		lists = append(lists[:i], lists[i+1:]...)
+	// 		w.WriteHeader(http.StatusOK)
+	// 		return
+	// 	}
+	// }
 
 	http.Error(w, "List not found", http.StatusNotFound)
 
 }
 
 func blogIDHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.Path)
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST,DELETE,OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	switch r.Method {
 	case http.MethodGet:
+		fmt.Println("get id req")
 		handleGetID(w)
 	case http.MethodPost:
+		fmt.Println("post  req")
 		handlePostID(w, r)
 	case http.MethodDelete:
+		fmt.Println("delete req")
 		handleDelID(w, r)
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
@@ -144,6 +258,26 @@ func handleGetID(w http.ResponseWriter) {
 	// w.Header().Set("Access-Control-Allow-Origin", "*")
 	// w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	// w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	var ids []ID
+	rows, err := db.Query("SELECT id, name FROM users")
+	if err != nil {
+		log.Printf("Error querying database: %v", err)
+		http.Error(w, "Error fetching data", http.StatusInternalServerError)
+		return
+	}
+
+	for rows.Next() {
+		var id ID
+		if err := rows.Scan(&id.ID, &id.Name); err != nil {
+
+			http.Error(w, "Error scanning data", http.StatusInternalServerError)
+			return
+		}
+		ids = append(ids, id)
+	}
+
+	defer rows.Close()
+
 	jsonData, err := json.Marshal(ids)
 	if err != nil {
 		http.Error(w, "Error marshalling JSON", http.StatusInternalServerError)
@@ -171,35 +305,61 @@ func handlePostID(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	mutex.Lock()
-	newID.ID = len(ids) + 1
-	ids = append(ids, newID)
-	mutex.Unlock()
+	err = db.QueryRow("INSERT INTO users (name) VALUES ($1) RETURNING id", newID.Name).Scan(&newID.ID)
+	if err != nil {
+		log.Printf("Error inserting data: %v", err)
+		http.Error(w, "Error inserting data", http.StatusInternalServerError)
+		return
+	}
+
+	// mutex.Lock()
+	// newID.ID = len(ids) + 1
+	// ids = append(ids, newID)
+	// mutex.Unlock()
 
 	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "Inserted ID: %d", newID.ID)
 }
 
 func handleDelID(w http.ResponseWriter, r *http.Request) {
 	// w.Header().Set("Access-Control-Allow-Origin", "*")
 	// w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
 	// w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	fmt.Println("Вызов handleDelID") // Отладочное сообщение
+	fmt.Println("URL.Path:", r.URL.Path)
 	idStr := strings.TrimPrefix(r.URL.Path, "/blog/id/")
 	id, err := strconv.Atoi(idStr)
+	fmt.Println(id)
 	if err != nil {
 		http.Error(w, "Invalid ID", http.StatusBadRequest)
 		return
 	}
 
-	mutex.Lock()
-	defer mutex.Unlock()
-
-	for i, ident := range ids {
-		if ident.ID == id {
-			ids = append(ids[:i], ids[i+1:]...)
-			w.WriteHeader(http.StatusOK)
-			return
-		}
+	result, err := db.Exec("DELETE FROM users WHERE id = $1", id)
+	if err != nil {
+		http.Error(w, "Error deleting data", http.StatusInternalServerError)
+		return
 	}
 
-	http.Error(w, "ID not found", http.StatusNotFound)
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		log.Printf("error :%v", err)
+		http.Error(w, "Error getting rows affected", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println(strconv.Itoa(int(rowsAffected)))
+	// mutex.Lock()
+	// defer mutex.Unlock()
+
+	// for i, ident := range ids {
+	// 	if ident.ID == id {
+	// 		ids = append(ids[:i], ids[i+1:]...)
+	// 		w.WriteHeader(http.StatusOK)
+	// 		return
+	// 	}
+	// }
+
+	// http.Error(w, "ID not found", http.StatusNotFound) ИНТЕРЕССССС
+
 }
