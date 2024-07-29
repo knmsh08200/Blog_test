e5a28d7cbce789f9d2313b0c7892f4e585a9ffd8
Mtvy <m.d.prudnik@gmail.com>
2024-07-29 19:40:57 +0300

    refact

--- blog.go
+++ blog.go
@@ -40,32 +40,42 @@ var (
 
 var db *sql.DB
 
+// выносим в cmd/main.go
 func main() {
+	//
+	// ко всему относится - подредачить название
+	// (должно быть читаемо но без лишних слов)
+	//
+
+	// init observability (metrics) (отдельный пакет в internal)
+	go Init(":8082")
+
+	// init db provider (отдельный пакет в internal)
 	var err error
 	db, err = sql.Open("postgres", "postgres://postgres:postgres@db:5432/postgres?sslmode=disable")
 	if err != nil {
 		log.Fatal("Error connecting to the database:", err)
 	}
 	fmt.Println("hello mtvy")
+
+	// убрать в .gracefull shutdown. (прогуглить) then (что-то context) (context.WithTimeout, WithCancel и тд)
+	// добавить context.WithTimeout на запросы в бд (убрать в env значение) (viper для чтения конфигов)
 	defer db.Close()
+
+	// init route handler (пусть пока лежит в файле internal/route.go)
 	// http.Handle("/", http.FileServer(http.Dir("./static")))
-
 	mux := http.NewServeMux()
-
 	// http.HandleFunc("/blog/list/", blogListHandler)
 	// http.HandleFunc("/blog/id/", blogIDHandler)
 	// http.HandleFunc("/blog/id/counter", blogCounterHandler)
 	mux.HandleFunc("/blog/list/", blogListHandler)
 	mux.HandleFunc("/blog/id/", blogIDHandler)
 	mux.HandleFunc("/blog/id/counter", blogCounterHandler)
-
-	go Init(":8082")
-
 	handler := MetricsMiddleware(mux)
 
 	// Start the server
-
 	log.Println("Server is listening on port 3001...")
+	// не забывай обрабатывать ошибки --->
 	log.Fatal(http.ListenAndServe(":3001", handler))
 }
 
